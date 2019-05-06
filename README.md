58e2c2263eb0cc2599a6be3a2591d4fe91d3dd17
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2019-05-06 14:56:17 +0200

    :tada: Init repository

---
+++ README.md
@@ -0,0 +1,3 @@
+# gitmoji-intellij-plugin
+
+Intellij plugin for add a button on the commit dialog to add gitmoji.
---
+++ src/main/kotlin/com/github/patou/gitmoji/GitCommitAction.kt
@@ -0,0 +1,10 @@
+package com.github.patou.gitmoji
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class GitCommitAction : AnAction() {
+    override fun actionPerformed(e: AnActionEvent) {
+
+    }
+}
