457572d9059e499f3f91d5fe0b1ff69f56ae5fae
Patrice De Saint Steban <patrice@desaintsteban.fr>
2020-01-02 16:48:31 +0100

    :sparkles: Fix #1 Allow to print unicode character (#2)

--- src/main/kotlin/com/github/patou/gitmoji/GitmojiData.kt
+++ src/main/kotlin/com/github/patou/gitmoji/GitmojiData.kt
@@ -3,9 +3,10 @@ package com.github.patou.gitmoji
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
-data class GitmojiData(val code: String, val description: String) {
+const val CONFIG_USE_UNICODE: String = "com.github.patou.gitmoji.use-unicode"
+data class GitmojiData(val code: String, val unicode: String, val description: String) {
     var icon : Icon = loadIcon(code)
-    var emotji : String = ":" + code + ":"
+    var emoji : String = ":" + code + ":"
 
     private fun loadIcon(code: String) : Icon {
         try {
