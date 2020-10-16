1369cdaf19ad376478f11a7b03bc7be93a7b5d3f
Patrice de Saint Steban <patrice.desaintsteban@zenika.com>
2020-10-16 17:56:43 +0200

    :lipstick: Use the emoji code instead of the icon.

--- src/main/kotlin/com/github/patou/gitmoji/GitmojiData.kt
+++ src/main/kotlin/com/github/patou/gitmoji/GitmojiData.kt
@@ -1,19 +1,6 @@
 package com.github.patou.gitmoji
 
-import com.intellij.openapi.util.IconLoader
-import javax.swing.Icon
-
 const val CONFIG_USE_UNICODE: String = "com.github.patou.gitmoji.use-unicode"
-data class GitmojiData(val code: String, val unicode: String, val description: String) {
-    var icon : Icon = loadIcon(code)
-    var emoji : String = ":" + code + ":"
-
-    private fun loadIcon(code: String) : Icon {
-        try {
-            return IconLoader.getIcon("/icons/" + code + ".png")
-        }
-        catch (e : Exception) {
-            return IconLoader.getIcon("/icons/anguished.png")
-        }
-    }
+data class GitmojiData(val name: String, val emoji: String, val description: String) {
+    var code : String = ":$name:"
 }
