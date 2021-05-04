09243e4d468976f39b888b670fc344e80875aca7
Patrice De Saint Steban <patrice.desaintsteban@zenika.com>
2021-05-04 11:57:06 +0200

    :sparkles: Add option for insert gitmoji in the carret position Fix #14 (#19)
    * :sparkles: Add option for insert gitmoji in the carret position Fix #14
    
    * :bookmark: Version 1.7.0

--- src/main/kotlin/com/github/patou/gitmoji/Gitmojis.kt
+++ src/main/kotlin/com/github/patou/gitmoji/Gitmojis.kt
@@ -9,5 +9,24 @@ class Gitmojis(val gitmojis: List<Gitmoji>) {
         fun defaultDisplayType(): String {
             return if (SystemInfo.isWindows) "icon" else "emoji"
         }
+
+        fun insertAt(target: String, position: Int, insert: String): String {
+            val targetLen = target.length
+            require(!(position < 0 || position > targetLen)) { "position=$position" }
+            if (insert.isEmpty()) {
+                return target
+            }
+            if (position == 0) {
+                return insert + target
+            } else if (position == targetLen) {
+                return target + insert
+            }
+            val insertLen = insert.length
+            val buffer = CharArray(targetLen + insertLen)
+            target.toCharArray(buffer, 0, 0, position)
+            insert.toCharArray(buffer, position, 0, insertLen)
+            target.toCharArray(buffer, position + insertLen, position, targetLen)
+            return String(buffer)
+        }
     }
 }
